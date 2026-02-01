d6e10686d62f5c0262f600ce5fac74e19b15b18a
ASWANTH A <159251572+Aswanth44@users.noreply.github.com>
2026-02-01 19:43:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("startBtn");
+const response = document.getElementById("response");
+
+btn.addEventListener("click", () => {
+  response.textContent = "Interesting choice. The developer is now responsible.";
+});
