bc84cf810a7e78d5ba3cd8cf816578807317443f
ASWANTH A <159251572+Aswanth44@users.noreply.github.com>
2026-02-01 19:42:33 +0530

    Delete devroaster directory

--- devroaster/script.js
+++
@@ -1,6 +0,0 @@
-const btn = document.getElementById("startBtn");
-const response = document.getElementById("response");
-
-btn.addEventListener("click", () => {
-  response.textContent = "Interesting choice. The developer is now responsible.";
-});
